0bbf5a4bbab515c7ec1077ecb052a16174f26d17
libresoul <contact@damindu.me>
2026-07-26 01:05:57 +0530

    feat: read from messages by 8 bytes and log them

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal("Failed to open file: ", err)
+	}
+
+	defer file.Close()
+
+	for {
+		buffer := make([]byte, 8)
+		_, err := file.Read(buffer)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			log.Fatal(err)
+		}
+		fmt.Printf("read: %s\n", string(buffer))
+	}
 }
