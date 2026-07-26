e48dc4a44f8e9570db0fe85d7c08574fb9aebe05
libresoul <contact@damindu.me>
2026-07-27 00:04:30 +0530

    refactor: move line read logic into reusable goroutine

--- main.go
+++ main.go
@@ -15,36 +15,50 @@ func main() {
 		log.Fatal("Failed to open file: ", err)
 	}
 
-	defer file.Close()
-	var currentLine string
-
-	for {
-		buffer := make([]byte, 8)
-		_, err := file.Read(buffer)
-
-		if !strings.Contains(string(buffer), "\n") {
-			currentLine += string(buffer)
-		}
-		parts := strings.Split(string(buffer), "\n")
-
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			log.Fatal(err)
-		}
-
-		for j, p := range parts {
-			if len(parts) == 1 {
-				continue
-			}
-			if j != len(parts)-1 {
-				fmt.Printf("read: %s\n", currentLine+p)
-				currentLine = ""
-				continue
-			}
-			currentLine += p
-		}
-
+	lineCh := getLinesChannel(file)
+	for line := range lineCh {
+		fmt.Println("read:", line)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer close(ch)
+		defer f.Close()
+
+		var currentLine string
+
+		for {
+			buffer := make([]byte, 8)
+			_, err := f.Read(buffer)
+
+			if !strings.Contains(string(buffer), "\n") {
+				currentLine += string(buffer)
+			}
+			parts := strings.Split(string(buffer), "\n")
+
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				log.Fatal(err)
+			}
+
+			for j, p := range parts {
+				if len(parts) == 1 {
+					continue
+				}
+				if j != len(parts)-1 {
+					ch <- currentLine + p
+					currentLine = ""
+					continue
+				}
+				currentLine += p
+			}
+		}
+	}()
+
+	return ch
 }
